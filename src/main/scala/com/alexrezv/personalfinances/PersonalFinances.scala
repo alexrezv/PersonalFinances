526284a96a494e1f745f4c64d6fe416c949da514
Alexander Rezvantsev <ramspb95@gmail.com>
2023-05-16 19:26:43 +0300

    simple api and app

--- src/main/scala/com/alexrezv/personalfinances/PersonalFinances.scala
+++ src/main/scala/com/alexrezv/personalfinances/PersonalFinances.scala
@@ -1,37 +1,25 @@
 package com.alexrezv.personalfinances
 
+import com.alexrezv.personalfinances.api.PersonalFinancesAPI
 import com.alexrezv.personalfinances.dao.repositories.impl.{IncomeRepositoryImpl, SpendingRepositoryImpl, UserRepositoryImpl}
-import com.alexrezv.personalfinances.dao.repositories.{IncomeRepository, SpendingRepository, UserRepository}
-import com.alexrezv.personalfinances.db.{dsLayer, quillLayer}
-import zio.http._
-import zio.{Console, _}
+import com.alexrezv.personalfinances.db.dsLayer
+import com.alexrezv.personalfinances.services.impl.UserServiceImpl
+import zio._
+import zio.http.Server
 
 object PersonalFinances extends ZIOAppDefault {
+  private val myEnv =
+    dsLayer >+>
+      UserRepositoryImpl.layer ++
+      IncomeRepositoryImpl.layer ++
+      SpendingRepositoryImpl.layer >+>
+      UserServiceImpl.layer
 
-  // Create HTTP route
-  val app: HttpApp[Any, Nothing] = Http.collect[Request] {
-    case Method.GET -> !! / "text" => Response.text("Hello World!")
-    case Method.GET -> !! / "json" => Response.json("""{"greetings": "Hello World!"}""")
-  }
+  private val httpApp = PersonalFinancesAPI.api
 
   // Run it like any simple app
-  override val run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] =
-    //    Server.serve(app).provide(Server.default)
-
-    UserRepository
-      .getUserByUUID("a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11")
-      .tapSome {
-        case Some(value) => Console.printLine(value)
-        case None        => Console.printLine("Nothing!")
-      }
-      .provide(quillLayer, dsLayer, UserRepositoryImpl.layer) *>
-      IncomeRepository
-        .getIncomeRecordsByUserId("a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11")
-        .tap(it => ZIO.foreach(it)(Console.printLine(_)))
-        .provide(quillLayer, dsLayer, IncomeRepositoryImpl.layer) *>
-      SpendingRepository
-        .getSpendingRecordsByUserId("a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11")
-        .tap(it => ZIO.foreach(it)(Console.printLine(_)))
-        .provide(quillLayer, dsLayer, SpendingRepositoryImpl.layer)
-        .exitCode
+  override val run: ZIO[Any & ZIOAppArgs & Scope, Any, Any] =
+    Server
+      .serve(httpApp)
+      .provide(Server.default, myEnv)
 }
