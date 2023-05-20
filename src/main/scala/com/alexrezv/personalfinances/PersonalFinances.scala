3aefbe0befc2a00694ac4b351eb857529bdcddb7
Alexander Rezvantsev <ramspb95@gmail.com>
2023-05-20 18:31:33 +0300

    app

--- src/main/scala/com/alexrezv/personalfinances/PersonalFinances.scala
+++ src/main/scala/com/alexrezv/personalfinances/PersonalFinances.scala
@@ -3,23 +3,26 @@ package com.alexrezv.personalfinances
 import com.alexrezv.personalfinances.api.PersonalFinancesAPI
 import com.alexrezv.personalfinances.dao.repositories.impl.{IncomeRepositoryImpl, SpendingRepositoryImpl, UserRepositoryImpl}
 import com.alexrezv.personalfinances.db.dsLayer
-import com.alexrezv.personalfinances.services.impl.UserServiceImpl
+import com.alexrezv.personalfinances.services.impl.{CryptoServiceImpl, UserServiceImpl}
 import zio._
+import zio.crypto.encryption.SymmetricEncryption
 import zio.http.Server
 
 object PersonalFinances extends ZIOAppDefault {
-  private val myEnv =
-    dsLayer >+>
-      UserRepositoryImpl.layer ++
-      IncomeRepositoryImpl.layer ++
-      SpendingRepositoryImpl.layer >+>
-      UserServiceImpl.layer
-
   private val httpApp = PersonalFinancesAPI.api
 
   // Run it like any simple app
   override val run: ZIO[Any & ZIOAppArgs & Scope, Any, Any] =
     Server
       .serve(httpApp)
-      .provide(Server.default, myEnv)
+      .provide(
+        Server.default,
+        dsLayer,
+        UserRepositoryImpl.layer,
+        IncomeRepositoryImpl.layer,
+        SpendingRepositoryImpl.layer,
+        SymmetricEncryption.live,
+        CryptoServiceImpl.layer,
+        UserServiceImpl.layer
+      )
 }
