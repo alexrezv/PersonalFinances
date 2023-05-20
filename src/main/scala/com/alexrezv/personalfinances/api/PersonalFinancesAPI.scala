67fb1c11c371ad9ea4b721866431700efebc9369
Alexander Rezvantsev <ramspb95@gmail.com>
2023-05-20 18:31:17 +0300

    simple auth for api

--- src/main/scala/com/alexrezv/personalfinances/api/PersonalFinancesAPI.scala
+++ src/main/scala/com/alexrezv/personalfinances/api/PersonalFinancesAPI.scala
@@ -1,21 +1,53 @@
 package com.alexrezv.personalfinances.api
 
+import com.alexrezv.personalfinances.dto.UserDTO
 import com.alexrezv.personalfinances.services.UserService
-import io.circe.syntax._
+import zio._
 import zio.http._
-import zio.{&, ZIO}
+import zio.json._
 
 import javax.sql.DataSource
 
 object PersonalFinancesAPI {
-  val api: Http[UserService & DataSource, Nothing, Request, Response] = Http.collectZIO[Request] {
+  private val authMiddleWare
+      : RequestHandlerMiddleware[Nothing, DataSource & UserService, Serializable, Boolean] =
+    RequestHandlerMiddlewares.customAuthZIO(
+      verify = headers =>
+        headers.get(Header.Authorization) match {
+          case Some(Header.Authorization.Basic("finan", "finan"))   => ZIO.succeed(true)
+          case Some(Header.Authorization.Basic(userName, password)) =>
+            for {
+              user <- UserService.findByLoginWithPassword(userName)
+              pass <- ZIO.fromOption(user.password)
+            } yield pass.equals(password)
+          case _                                                    => ZIO.succeed(false)
+        }
+    )
+
+  // compose basic auth, request/response logging, timeouts middlewares
+  val composedMiddlewares = /*RequestHandlerMiddlewares.basicAuth("finan", "finan") ++*/
+    authMiddleWare ++
+      RequestHandlerMiddlewares.debug ++
+      RequestHandlerMiddlewares.timeout(5.seconds)
+
+  val api = Http.collectZIO[Request] {
+    case request @ Method.POST -> !! / "user" =>
+      (for {
+        body   <- request.body.asString(Charsets.Utf8)
+        dto    <- ZIO.fromEither(UserDTO.decoder.decodeJson(body))
+        result <- UserService.create(dto)
+      } yield result).foldZIO(
+        _ => ZIO.succeed(Response.status(Status.BadRequest)),
+        res => ZIO.succeed(Response.json(res))
+      )
+
     //list users
     case Method.GET -> !! / "users" =>
       UserService
         .findAll()
         .foldZIO(
           _ => ZIO.succeed(Response.status(Status.NotFound)),
-          res => ZIO.succeed(Response.json(res.asJson.toString()))
+          res => ZIO.succeed(Response.json(res.toJson))
         )
 
     // get user by it's uuid
@@ -24,7 +56,7 @@ object PersonalFinancesAPI {
         .findByUUID(uuid)
         .foldZIO(
           _ => ZIO.succeed(Response.status(Status.NotFound)),
-          res => ZIO.succeed(Response.json(res.asJson.toString()))
+          res => ZIO.succeed(Response.json(res.toJson))
         )
 
     // list user's incomes
@@ -33,7 +65,7 @@ object PersonalFinancesAPI {
         .findIncomes(uuid)
         .foldZIO(
           _ => ZIO.succeed(Response.status(Status.NotFound)),
-          res => ZIO.succeed(Response.json(res.asJson.toString()))
+          res => ZIO.succeed(Response.json(res.toJson))
         )
 
     // list user's spendings
@@ -42,8 +74,8 @@ object PersonalFinancesAPI {
         .findSpendings(uuid)
         .foldZIO(
           _ => ZIO.succeed(Response.status(Status.NotFound)),
-          res => ZIO.succeed(Response.json(res.asJson.toString()))
+          res => ZIO.succeed(Response.json(res.toJson))
         )
 
-  }
+  } @@ composedMiddlewares
 }
