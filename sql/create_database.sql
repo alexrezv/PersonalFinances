f5de9a7bef35549c2a4c146bbdc329659625791a
Alexander Rezvantsev <ramspb95@gmail.com>
2023-05-20 18:31:42 +0300

    db script

--- sql/create_database.sql
+++ sql/create_database.sql
@@ -13,17 +13,20 @@ drop table if exists public.usr;
 
 create table public.usr
 (
-    id       serial      not null
+    id         serial       not null
         constraint usr_pk primary key,
-    "uuid"   uuid        not null
+    "uuid"     uuid         not null
         constraint usr_guid_unique unique,
-    username varchar(16) not null
+    username   varchar(16)  not null
+        constraint usr_username_unique unique,
+    "password" varchar(128) not null
 );
 
 alter table public.usr owner to finan;
 
-insert into public.usr("uuid", username) values
-    ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'john_doe');
+-- insert into public.usr("uuid", username) values
+--     ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'john_doe'),
+--     ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12', 'jane_doe');
 
 
 -- income_category type definition
@@ -49,11 +52,11 @@ create table public.income
 
 alter table public.income owner to finan;
 
-insert into public.income(usr_id, "date", amount, "category", "comment") values
-    ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', '2023-01-20', 10000, 'Salary', null),
-    ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', '2023-02-20', 10000, 'Salary', null),
-    ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', '2023-03-20', 10000, 'Salary', null),
-    ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', '2023-03-20', 2000, 'Bonus', 'First quarter bonus');
+-- insert into public.income(usr_id, "date", amount, "category", "comment") values
+--     ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', '2023-01-20', 10000, 'Salary', null),
+--     ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', '2023-02-20', 10000, 'Salary', null),
+--     ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', '2023-03-20', 10000, 'Salary', null),
+--     ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', '2023-03-20', 2000, 'Bonus', 'First quarter bonus');
 
 
 -- spending_category type definition
@@ -80,10 +83,10 @@ create table public.spending
 
 alter table public.spending owner to finan;
 
-insert into public.spending(usr_id, "date", amount, "category", "comment") values
-    ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', '2023-01-21', 1000, 'Rent', null),
-    ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', '2023-01-21', 100, 'Bills', null),
-    ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', '2023-02-21', 1000, 'Rent', null),
-    ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', '2023-02-21', 100, 'Bills', null),
-    ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', '2023-03-21', 1000, 'Rent', null),
-    ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', '2023-03-21', 100, 'Bills', null);
+-- insert into public.spending(usr_id, "date", amount, "category", "comment") values
+--     ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', '2023-01-21', 1000, 'Rent', null),
+--     ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', '2023-01-21', 100, 'Bills', null),
+--     ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', '2023-02-21', 1000, 'Rent', null),
+--     ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', '2023-02-21', 100, 'Bills', null),
+--     ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', '2023-03-21', 1000, 'Rent', null),
+--     ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', '2023-03-21', 100, 'Bills', null);
